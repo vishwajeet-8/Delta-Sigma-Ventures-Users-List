eace01b87f545bc2f54597f25c25ca8edd1915cb
vishwajeet-8 <vishwajeetrout8@gmail.com>
2026-02-09 00:42:17 +0530

    add api

--- src/api/userServices.js
+++ src/api/userServices.js
@@ -13,33 +13,54 @@ async function request(url, options = {}) {
   return res.status !== 204 ? res.json() : null;
 }
 
+const devurl = "http://localhost:3000";
+const url = "https://6988ccb3780e8375a6892f12.mockapi.io/api/users";
+
 // ADD USER MOCK API
+//DEVELOPMENT
+// export function addUser(formData) {
+//   return request(url, {
+//     method: "POST",
+//     body: JSON.stringify({
+//       ...formData,
+//       id: crypto.randomUUID(),
+//     }),
+//   });
+// }
+
+//PRODUCTION
 export function addUser(formData) {
-  return request("http://localhost:3000/users", {
+  return request(url, {
     method: "POST",
-    body: JSON.stringify({
-      ...formData,
-      id: crypto.randomUUID(),
-    }),
+    body: JSON.stringify(formData),
   });
 }
 
 // GET ALL USERS MOCK API
 export function getAllUser() {
-  return request("http://localhost:3000/users");
+  return request(url);
 }
 
 // UPDATE USERS MOCK API
+//DEVELOPMENT
+// export function updateUser(id, formData) {
+//   return request(`http://localhost:3000/users/${id}`, {
+//     method: "PATCH",
+//     body: JSON.stringify(formData),
+//   });
+// }
+
+//PRODUCTION
 export function updateUser(id, formData) {
-  return request(`http://localhost:3000/users/${id}`, {
-    method: "PATCH",
+  return request(`url/${id}`, {
+    method: "PUT",
     body: JSON.stringify(formData),
   });
 }
 
 //DELETE USERS MOCK API
 export function deleteUser(id) {
-  return request(`http://localhost:3000/users/${id}`, {
+  return request(`url/${id}`, {
     method: "DELETE",
   });
 }
